a9e5791ba547203cb8d426678a4961d53da5374a
jason <642460897@qq.com>
2020-08-05 20:25:42 +0800

    新增吸附边界

--- library/src/main/java/com/lieni/library/easyfloat/view/FloatView.java
+++ library/src/main/java/com/lieni/library/easyfloat/view/FloatView.java
@@ -1,8 +1,8 @@
 package com.lieni.library.easyfloat.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.ViewGroup;
 import android.view.ViewPropertyAnimator;
@@ -12,7 +12,11 @@ import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.lieni.library.easyfloat.R;
+
 public class FloatView extends FrameLayout {
+    private int marginLeft,marginRight,marginTop,marginBottom;
+
     public FloatView(@NonNull Context context) {
         this(context,null);
     }
@@ -23,9 +27,20 @@ public class FloatView extends FrameLayout {
 
     public FloatView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(attrs);
     }
-    private void init(){
+    private void init(@Nullable AttributeSet attrs){
+        TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.FloatView);
+        int margin=typedArray.getLayoutDimension(R.styleable.FloatView_float_margin,0);
+        int horizontalMargin=typedArray.getLayoutDimension(R.styleable.FloatView_float_marginHorizontal,margin);
+        int verticalMargin=typedArray.getLayoutDimension(R.styleable.FloatView_float_marginVertical,margin);
+
+        marginLeft=typedArray.getLayoutDimension(R.styleable.FloatView_float_marginStart,horizontalMargin);
+        marginRight=typedArray.getLayoutDimension(R.styleable.FloatView_float_marginEnd,horizontalMargin);
+        marginTop=typedArray.getLayoutDimension(R.styleable.FloatView_float_marginTop,verticalMargin);
+        marginBottom=typedArray.getLayoutDimension(R.styleable.FloatView_float_marginBottom,verticalMargin);
+
+        typedArray.recycle();
         setClickable(true);
     }
 
@@ -40,24 +55,12 @@ public class FloatView extends FrameLayout {
     }
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
-        Log.i("sssssssss","dispatchTouchEvent");
-        switch (ev.getAction()){
-            case MotionEvent.ACTION_DOWN:
-//                pressedTime=System.currentTimeMillis();
-                Log.i("sssssssss","ACTION_DOWN");
-                break;
-            case MotionEvent.ACTION_UP:
-                Log.i("sssssssss","ACTION_UP");
-                break;
-        }
         handleEvent(ev);
         return super.dispatchTouchEvent(ev);
     }
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-        Log.i("sssssssss","onInterceptTouchEvent");
-
         return super.onInterceptTouchEvent(ev);
     }
 
@@ -81,7 +84,6 @@ public class FloatView extends FrameLayout {
         int rawY = (int) event.getRawY();
         switch (event.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN:
-                Log.i("sssssssss","onTouchEvent ACTION_DOWN");
                 setPressed(true);
                 isDrag = false;
                 getParent().requestDisallowInterceptTouchEvent(true);
@@ -120,37 +122,68 @@ public class FloatView extends FrameLayout {
                 if (isDrag) {
                     //恢复按压效果
                     setPressed(false);
-                    boolean xLeftOver=getX()<0;
-                    boolean xRightOver=getX()+getMeasuredWidth()>parentWidth;
-                    boolean yTopOver=getY()<0;
-                    boolean yBottomOver=getY()+getMeasuredHeight()>parentHeight;
+                    boolean xLeftOver=getX()<marginLeft;
+                    boolean xRightOver=getX()>parentWidth-marginRight-getMeasuredWidth();
+                    boolean yTopOver=getY()<marginTop;
+                    boolean yBottomOver=getY()>parentHeight-marginBottom-getMeasuredHeight();
 
                     if (xLeftOver||xRightOver||yTopOver||yBottomOver ) {
                         //吸附
                         ViewPropertyAnimator animator= animate().setInterpolator(new DecelerateInterpolator()).setDuration(500);
                         if(xLeftOver||xRightOver){
                             if(xLeftOver) {
-                                animator.x(0);
+                                animator.x(marginLeft);
                             }
                             if(xRightOver){
-                                animator.x(parentWidth-getMeasuredWidth());
+                                animator.x(parentWidth-marginRight-getMeasuredWidth());
                             }
                         }
                         if(yTopOver||yBottomOver){
                             if(yTopOver){
-                                animator.y(0);
+                                animator.y(marginTop);
                             }
                             if(yBottomOver){
-                                animator.y(parentHeight-getMeasuredHeight());
+                                animator.y(parentHeight-marginBottom-getMeasuredHeight());
                             }
                         }
                         animator.start();
-
                     }
                 }
                 break;
             default:
                 break;
         }
+    }
+
+    public int getMarginLeft() {
+        return marginLeft;
+    }
+
+    public void setMarginLeft(int marginLeft) {
+        this.marginLeft = marginLeft;
+    }
+
+    public int getMarginRight() {
+        return marginRight;
+    }
+
+    public void setMarginRight(int marginRight) {
+        this.marginRight = marginRight;
+    }
+
+    public int getMarginTop() {
+        return marginTop;
+    }
+
+    public void setMarginTop(int marginTop) {
+        this.marginTop = marginTop;
+    }
+
+    public int getMarginBottom() {
+        return marginBottom;
+    }
+
+    public void setMarginBottom(int marginBottom) {
+        this.marginBottom = marginBottom;
     }
 }
